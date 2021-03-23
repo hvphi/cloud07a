bb4854c192ef4dc50c85b4b060602a35a94a908a
Van Phi <Van Phi@172.16.69.133>
2021-03-23 14:01:42 +0700

    Them Landing page

--- index.php
+++ index.php
@@ -36,6 +36,9 @@
              <li class="nav-item">
                  <a class="nav-link" href="Web/LogOut.php">Logout</a>
             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="Web/phanpage.html">Plan page</a>
+            </li>
          </ul>
 
       </div>
