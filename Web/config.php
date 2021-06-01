9ff787eab277c46d1716399249568e2efed3ebe3
Van Phi <Van Phi@172.16.69.133>
2021-06-01 14:02:43 +0700

    Ket noi csdl postgresql

--- Web/config.php
+++ Web/config.php
@@ -1,17 +1,20 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'shoeshop');
+define('DB_SERVER', 'ec2-54-152-185-191.compute-1.amazonaws.com');
+define('DB_USERNAME', 'zwkuejqunqscnu');
+define('DB_PASSWORD', '6f099a8d7eb7ded73e6188f700ed32bec813e127840809300e3a5f307f4f46a4');
+define('DB_NAME', 'd7k6ilbd621ucp');
+define('DB_PORT', '5432');
  
 /* Attempt to connect to MySQL database */
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = pg_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+else
+    echo 'Connect seuccesfully';
 ?>
